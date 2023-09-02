beee79c9a05b26e76d3d6efa01b6149b77673841
Younganswer <dudtj1999@gmail.com>
2023-09-02 21:39:47 +0900

    [Feat] SCRUM-61 배포 환경 세팅 #13

--- src/service/nginx/tool/setup.sh
+++ src/service/nginx/tool/setup.sh
@@ -2,12 +2,12 @@
 
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out $CERTS_ -subj "/C=MO/L=KH/O=1337/OU=student/CN=$DOMAIN_NAME"
 
-sed -i "s/react_port/$REACT_PORT/g" /etc/nginx/sites-available/default.conf
-sed -i "s/nestjs_port/$NESTJS_PORT/g" /etc/nginx/sites-available/default.conf
-sed -i "s/my_domain/$DOMAIN_NAME/g" /etc/nginx/sites-available/default.conf
-sed -i "s/my_certs/$CERTS_/g" /etc/nginx/sites-available/default.conf
+sed -i "s|react_port|$REACT_PORT|g" /etc/nginx/sites-available/default
+sed -i "s|nestjs_port|$NESTJS_PORT|g" /etc/nginx/sites-available/default
+sed -i "s|my_domain|$DOMAIN_NAME|g" /etc/nginx/sites-available/default
+sed -i "s|my_certs|$CERTS_|g" /etc/nginx/sites-available/default
 
-cat /etc/nginx/sites-available/default.conf
+cat /etc/nginx/sites-available/default
 
 #sed -i '40s/.*/\tlog_format debug '\''$remote_addr - $remote_user [$time_local] '\'' '\''"$request" $status $body_bytes_sent '\'' '\''"$http_referer" "$http_user_agent"'\'';/' /etc/nginx/nginx.conf
 #sed -i '41s/.*/\taccess_log \/var\/log\/nginx\/access.log debug;/' /etc/nginx/nginx.conf
