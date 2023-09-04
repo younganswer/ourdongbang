7d5a3ddd0485228e156a8b3895a4f01b7828cb0e
Younganswer <dudtj1999@gmail.com>
2023-09-04 15:05:21 +0900

    [Feat] SCRUM-63 API documentation with swagger #17

--- src/service/nestjs/src/main.ts
+++ src/service/nestjs/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { setupSwagger } from 'util/swagger/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
+	setupSwagger(app);
 	await app.listen(3000);
 }
 bootstrap();
