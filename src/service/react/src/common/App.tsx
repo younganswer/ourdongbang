0907f21ac7e0a3e09e5b3a593ee44e1f9ae49102
jeongahn <inetty@naver.com>
2023-09-13 20:19:32 +0900

    pages -> page 폴더명 변경, PreviewPage import 폴더경로 수정

--- src/service/react/src/common/App.tsx
+++ src/service/react/src/common/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 // import logo from "./logo.svg";
-import PreviewPage from 'pages/PreviewPage';
+import PreviewPage from 'page/PreviewPage';
 import PToolBar from 'components/PToolBar';
 import { ToastContainer } from 'react-toastify';
 
