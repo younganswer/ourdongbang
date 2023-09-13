7295b9a3ac2113e4b2ac3c1dc0a6be9ff6733dc8
jeongahn <inetty@naver.com>
2023-09-13 19:42:37 +0900

    폴더 추가 및 홈버튼 눌렀을때 PreviewPage구현

---
+++ src/service/react/src/components/CustomInput.tsx
@@ -0,0 +1,23 @@
+import React, { ChangeEvent } from 'react';
+
+interface CustomInputProps {
+	label: string;
+	value: string;
+	setValue: (value: string) => void;
+	type?: string;
+}
+
+const CustomInput: React.FC<CustomInputProps> = ({ label, value, setValue, type = 'text' }) => {
+	const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setValue(e.target.value);
+	};
+
+	return (
+		<div>
+			<label style={{ color: 'black' }}>{label}</label>
+			<input style={{ width: '97%', marginBottom: 30 }} value={value} type={type} onChange={handleInputChange} />
+		</div>
+	);
+};
+
+export default CustomInput;
---
+++ src/service/react/src/pages/PreviewPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../components/HeadLine.css';
+const PreviewPage = () => {
+	//
+	return (
+		<div>
+			<div className="headline"></div>
+		</div>
+	);
+};
+
+export default PreviewPage;
