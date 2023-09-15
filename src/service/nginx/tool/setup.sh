412012c87005d7c436e1386c56b2556c6436e2a8
Younganswer <dudtj1999@gmail.com>
2023-09-15 11:52:36 +0900

    [Feat] dockerize reciept service #49
    receipt AI flask test를 위해POST method /receipt API 작성
    form-data로 imageId 전송
    flask에서 parsing 필요

--- src/service/nginx/tool/setup.sh
+++ src/service/nginx/tool/setup.sh
@@ -8,8 +8,6 @@ sed -i "s|my_domain|$DOMAIN_NAME|g" /etc/nginx/sites-available/default
 sed -i "s|my_certs_key|$CERTS_KEY|g" /etc/nginx/sites-available/default
 sed -i "s|my_certs|$CERTS|g" /etc/nginx/sites-available/default
 
-cat /etc/nginx/sites-available/default
-
 #sed -i '40s/.*/\tlog_format debug '\''$remote_addr - $remote_user [$time_local] '\'' '\''"$request" $status $body_bytes_sent '\'' '\''"$http_referer" "$http_user_agent"'\'';/' /etc/nginx/nginx.conf
 #sed -i '41s/.*/\taccess_log \/var\/log\/nginx\/access.log debug;/' /etc/nginx/nginx.conf
 #sed -i '42s/.*/\terror_log \/var\/log\/nginx\/error.log debug;/' /etc/nginx/nginx.conf
