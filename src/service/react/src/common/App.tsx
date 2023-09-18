a07ab12e0550cf01e007bf8fdd44e5d227025a2b
jeongahn <inetty@naver.com>
2023-09-18 16:32:27 +0900

    context 폴더 생성 및 authcontext 파일 생성, calenderpage, auditpage 생성 및 연결

--- src/service/react/src/common/App.tsx
+++ src/service/react/src/common/App.tsx
@@ -3,9 +3,11 @@ import { Routes, Route } from 'react-router-dom';
 // import logo from "./logo.svg";
 import PreviewPage from 'page/PreviewPage';
 import LoginPage from 'page/LoginPage';
+import RegisterPage from 'page/RegisterPage';
+import AuditPage from 'page/AuditPage';
+import CalenderPage from 'page/CalenderPage';
 import PToolBar from 'components/PToolBar';
 import { ToastContainer } from 'react-toastify';
-import RegisterPage from 'page/RegisterPage';
 
 function App() {
 	return (
@@ -16,6 +18,8 @@ function App() {
 				<Route path="/" element={<PreviewPage />} />
 				<Route path="/auth/login" element={<LoginPage />} />
 				<Route path="/auth/register" element={<RegisterPage />} />
+				<Route path="/main/calender" element={<CalenderPage />} />
+				<Route path="/main/audit" element={<AuditPage />} />
 			</Routes>
 		</div>
 	);
---
+++ src/service/react/src/page/AuditPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AuditPage = () => {
+	return (
+		<div>
+			<h3>calenderpage</h3>
+		</div>
+	);
+};
+
+export default AuditPage;
