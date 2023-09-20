521d054c260859f8f31608bb7db75a7bd535f130
jeongahn <inetty@naver.com>
2023-09-21 02:39:43 +0900

    PToolBar (로그인전 ToolBar) button 배치 및 수정

--- src/service/react/src/components/PToolBar.tsx
+++ src/service/react/src/components/PToolBar.tsx
@@ -1,19 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../style/ToolBar.css';
+// TopToolBar.tsx
 
-const PToolBar = () => {
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom'; // 라우터 사용에 따라 변경
+import ToolBarBtn from './ToolBarBtn';
+import '../style/ToolBar.css'; // CSS 스타일 파일 경로에 따라 변경
+
+const PToolBar: React.FC = () => {
+	const [isScrolled, setIsScrolled] = useState(false);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			if (window.scrollY > 0) {
+				setIsScrolled(true);
+			} else {
+				setIsScrolled(false);
+			}
+		};
+
+		window.addEventListener('scroll', handleScroll);
+
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
+
 	return (
-		<div className="font-change">
-			<Link to="/" style={{ textDecoration: 'none' }}>
-				<span style={{ color: 'black', marginLeft: 10, fontSize: 40 }}>우리동방</span>
+		<div className={`top-toolbar${isScrolled ? ' scrolled' : ''}`}>
+			<Link to="/" className="toolbar-link">
+				<span className="toolbar-title">우리동방</span>
 			</Link>
-			<Link to="/auth/login">
-				<span style={{ float: 'right', color: 'black' }}>login</span>
-			</Link>
-			<Link to="/auth/register">
-				<span style={{ float: 'right', color: 'black', marginRight: 10 }}>Register</span>
-			</Link>
+			<div className="Ptoolbar-buttons">
+				<ToolBarBtn to="/auth/login" label="로그인" />
+				<ToolBarBtn to="/auth/register" label="회원가입" />
+			</div>
 		</div>
 	);
 };
