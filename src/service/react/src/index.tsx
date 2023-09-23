16b8dc1057fdc4bff226aaee9c99ffc58cfd191e
jeongahn <inetty@naver.com>
2023-09-23 19:11:59 +0900

    index.tsx 파일 authprovider 추가, registerpage 서버 연동

--- src/service/react/src/index.tsx
+++ src/service/react/src/index.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from 'common/App';
 // import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from 'context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<App />
+			<AuthProvider>
+				<App />
+			</AuthProvider>
 		</BrowserRouter>
 	</React.StrictMode>,
 );
