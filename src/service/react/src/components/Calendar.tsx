47bea7d124c14438ec81b9561c419ee02aabfd51
SeongJeongKyu <juku1133@gmail.com>
2023-10-01 23:09:28 +0900

    [Feat][SCRUM-183] 일정 등록 버튼 #127

--- src/service/react/src/components/Calendar.tsx
+++ src/service/react/src/components/Calendar.tsx
@@ -15,6 +15,18 @@ interface Event {
 const Calendar: React.FC<CalendarProps> = ({ events }) => {
 	const calendarRef = useRef<FullCalendar>(null);
 
+	const handleAddEventClick = () => {
+		// TODO: 이벤트 추가 로직을 여기에 구현
+		console.log('일정 추가 버튼이 클릭되었습니다.');
+	};
+
+	const customButtons = {
+		myCustomButton: {
+			text: '일정등록',
+			click: handleAddEventClick,
+		},
+	};
+
 	useEffect(() => {
 		if (calendarRef.current) {
 			calendarRef.current.getApi().addEventSource(events);
@@ -27,6 +39,16 @@ const Calendar: React.FC<CalendarProps> = ({ events }) => {
 			plugins={[dayGridPlugin]}
 			initialView="dayGridMonth"
 			events={events}
+			customButtons={customButtons}
+			buttonText={{
+				today: '오늘날짜', // "Today" 버튼 텍스트를 한국어로 변경
+			}}
+			headerToolbar={{
+				left: 'prev,next',
+				center: 'title',
+				right: 'today myCustomButton',
+			}}
+			locale="ko"
 		/>
 	);
 };
