c71da5df4d4ef77886ba6a7f0f7d65852725f8d0
yoon2000 <yoonhs02@kookmin.ac.kr>
2023-10-05 10:50:07 +0900

    [Feat] [SCRUM-215] club page 만들고 연결 하기 #139

---
+++ src/service/react/src/page/club/member/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import { AuthContext } from 'context/AuthContext';
+
+const Member = () => {
+	// 부원 정보를 표시하기 위한 데이터 가져오기 또는 렌더링 로직 추가
+
+	return (
+		<div>
+			{/* 부원 정보를 JSX로 표시합니다. */}
+			<p>부원 정보 내용...</p>
+		</div>
+	);
+};
+
+export default Member;
