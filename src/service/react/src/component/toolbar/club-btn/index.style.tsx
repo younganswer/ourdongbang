2de566bff405c844c0975852483b49a4ba568568
Younganswer <dudtj1999@gmail.com>
2023-10-14 21:14:30 +0900

    [Feat] [SCRUM-254] Profile component 디자인 확정 #167

--- src/service/react/src/component/toolbar/club-btn/index.style.tsx
+++ src/service/react/src/component/toolbar/club-btn/index.style.tsx
@@ -2,13 +2,13 @@ import { css } from '@emotion/css';
 
 export const ClubBtnStyle = css({
 	gridColumn: 5,
-	width: '46px',
-	height: '46px',
+	width: 46,
+	height: 46,
 	position: 'relative',
 
 	'> div:nth-child(1)': {
-		width: '46px',
-		height: '46px',
+		width: 46,
+		height: 46,
 		borderRadius: '70%',
 		border: '1px solid black',
 		backgroundColor: '#D9D9D9',
--- src/service/react/src/component/toolbar/club-btn/index.tsx
+++ src/service/react/src/component/toolbar/club-btn/index.tsx
@@ -14,21 +14,23 @@ const slideModal = (divRef: RefObject<HTMLDivElement>, isClicked: boolean, me: M
 		const clubBtnSwitch = divRef.current.children[1] as HTMLDivElement;
 
 		if (isClicked) {
-			clubBtnSwitch.style.transitionProperty = 'width, opacity, height';
-			clubBtnSwitch.style.transitionDelay = '0s, 0.225s, 0.4s';
-			clubBtnSwitch.style.transitionDuration = '0.5s, 0.3s, 0.5s';
+			clubBtnSwitch.style.transitionProperty = 'pointerEvents, width, opacity, height';
+			clubBtnSwitch.style.transitionDelay = '0s, 0s, 0.3s, 0.4s';
+			clubBtnSwitch.style.transitionDuration = '0s, 0.5s, 0.3s, 0.5s';
 			clubBtnSwitch.style.transitionTimingFunction = 'ease-in-out';
+			clubBtnSwitch.style.pointerEvents = 'auto';
 			clubBtnSwitch.style.width = '250px';
 			clubBtnSwitch.style.height = height;
 			clubBtnSwitch.style.opacity = '1';
 		} else {
-			clubBtnSwitch.style.transitionProperty = 'height, width, opacity';
-			clubBtnSwitch.style.transitionDelay = '0s, 0.3s, 0.35s';
-			clubBtnSwitch.style.transitionDuration = '0.4s, 0.5s, 0.2s';
+			clubBtnSwitch.style.transitionProperty = 'height, width, opacity, pointerEvents';
+			clubBtnSwitch.style.transitionDelay = '0s, 0.3s, 0.3s, 1s';
+			clubBtnSwitch.style.transitionDuration = '0.4s, 0.5s, 0.2s, 0s';
 			clubBtnSwitch.style.transitionTimingFunction = 'ease-in-out';
 			clubBtnSwitch.style.width = '0';
 			clubBtnSwitch.style.height = '70px';
 			clubBtnSwitch.style.opacity = '0';
+			clubBtnSwitch.style.pointerEvents = 'none';
 		}
 	}
 };
@@ -42,9 +44,7 @@ const Profile = (props: { club: Club | null; setIsClicked: Dispatch<SetStateActi
 			onClick={() => {
 				setIsClicked(true);
 			}}
-		>
-			{/*{!club ? '+' : club.name}*/}
-		</div>
+		></div>
 	);
 	console.log(club);
 };
