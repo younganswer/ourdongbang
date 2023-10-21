7d4670bf93790c31eebb518af6c362dee9c37042
younganswer <dudtj1999@gmail.com>
2023-10-21 08:34:40 +0000

    receipt OCR

--- src/service/receipt/src/app.py
+++ src/service/receipt/src/app.py
@@ -3,64 +3,72 @@ from flask import Flask, request, render_template, Response, make_response
 import requests, os, uuid, time, json, base64
 from werkzeug.utils import secure_filename
 from urllib.parse import quote
-
+import tempfile
 
 app = Flask(__name__)
-
-# 업로드된 이미지를 저장할 디렉토리 설정
-UPLOAD_FOLDER = "./uploads"
-app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
-
 
 def remove_space(str):
 	return str.replace(" ", "")
 
 def load_image(imageId):
-	url = os.environ['S3_BUCKET_URL_PREFIX'] + '/' + imageId
-	response = requests.get(url)
-	if response.status_code == 200:
-		filename = os.path.join(app.config['UPLOAD_FOLDER'],'receipt_image.jpg')
-		with open(filename, 'wb') as f:
-			f.write(response.content)
-		return 200
-	else:
-		print("Failed to download the image. Status code:", response.status_code)
-		return make_response(response.status_code)
+	url = os.environ['S3_BUCKET_PREFIX_URL'] + '/' + imageId
 	
-def receipt_recognition(code):
-	if code == 404 or code == 500:
-		return code
-	path = os.path.join(app.config['UPLOAD_FOLDER'],'receipt_image.jpg')
-	with open(path,'rb') as f:
+	response = requests.get(url)
+	if response.status_code != 200:
+		print("Failed to download the image")
+		return make_response(response.data, response.status_code)
+
+	temp_dir = tempfile.mkdtemp(dir=os.environ['TMPDIR'], prefix='receipt_')
+	temp_file = tempfile.mktemp(dir=temp_dir, suffix='.jpg')
+	with open(temp_file, 'wb') as f:
+		f.write(response.content)
+
+	return make_response(json.dumps(temp_file), 200)
+	
+def receipt_recognition(temp_file):
+	with open(temp_file, 'rb') as f:
 		file_data = f.read()
-	api_url = os.environ["CLOVA_API_URL"]
-	secret_key = os.environ["CLOVA_API_SECRET_KEY"]
+
 	request_json = {
-		"images": [{"format": "jpeg", "name": "demo",'data': base64.b64encode(file_data).decode()}],
+		"images": [{
+			"format": "jpg",
+			"name": "demo",
+			"data": base64.b64encode(file_data).decode()
+		}],
 		"requestId": str(uuid.uuid4()),
 		"version": "V2",
 		"timestamp": int(round(time.time() * 1000)),
 	}
 
+	os.remove(temp_file)
+	os.rmdir(os.path.dirname(temp_file))
+
 	payload = json.dumps(request_json).encode("UTF-8")
 
-	headers = {"X-OCR-SECRET": secret_key,'Content-Type': 'application/json'}
+	headers = {
+		"X-OCR-SECRET": os.environ["CLOVA_API_SECRET_KEY"],
+		'Content-Type': 'application/json'
+	}
 
-	response= requests.request("POST", api_url, headers=headers, data=payload)
-	Clova_result = response.json()
-	payment_info = Clova_result["images"][0]["receipt"]["result"]["paymentInfo"]
+	response = requests.request("POST", os.environ["CLOVA_API_URL"], headers=headers, data=payload)
+	if response.status_code != 200:
+		print("Failed to request OCR")
+		return make_response(json.dumps(response.json()), response.status_code)
+
+	response = response.json()
+	print(response)
+
+	payment_info = response["images"][0]["receipt"]["result"]["paymentInfo"]
 	payment_date = payment_info["date"]["text"]  # 날짜
 	payment_time = remove_space(payment_info["time"]["text"])  # 시간
-
 	store_name = remove_space(
-		Clova_result["images"][0]["receipt"]["result"]["storeInfo"]["name"]["text"]
+		response["images"][0]["receipt"]["result"]["storeInfo"]["name"]["text"]
 	)  # 상호명
 	total_price = remove_space(
-		Clova_result["images"][0]["receipt"]["result"]["totalPrice"]["price"][
+		response["images"][0]["receipt"]["result"]["totalPrice"]["price"][
 			"formatted"
 		]["value"]
 	)  # 금액
-
 
 	parsed_data = {
 		"dateOfPayment": payment_date,
@@ -69,21 +77,25 @@ def receipt_recognition(code):
 		"price": total_price,
 	}
 	
-	return parsed_data
-
+	return make_response(json.dumps(parsed_data), 200)
 
 @app.route("/receipt", methods=["POST"])
 def receipt_OCR():
 	imageId = request.form['imageId']
-	parsed_data = receipt_recognition(load_image(imageId))
-	if parsed_data == 404 or parsed_data == 500:
-		return Response(500)
+	
+	response = load_image(imageId)
+	if response.status_code != 200:
+		return Response(response.data, response.status_code)
 
-	#print(parsed_data)
-	response = Response(json.dumps(parsed_data),200)
+	temp_file = json.loads(response.data)
+
+	response = receipt_recognition(temp_file)
+
+	response = Response(response.data, response.status_code)
 	response.headers["Access-Control-Allow-Origin"] = "https://nestjs:3000"
-	print(response.data)
-	print(response.headers)
+
+	print(json.loads(response.data))
+
 	return response
 
 if __name__ == "__main__":
